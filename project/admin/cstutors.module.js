f8efd1b1870eaad232790e423e0df5acc1ba7a1d
Lillian Pollard <lillian-pollard@uiowa.edu>
2018-04-09 21:46:09 -0500

    admin files

---
+++ project/admin/cstutors.module.js
@@ -0,0 +1,9 @@
+/*
+* Creating a module for the soccer app
+* We'll be using the name of the app 'soccer' in the controller, and in the html (in the html element itself)
+*/
+(function() {
+    'use strict';
+    angular.module("cstutors", []);
+}
+)();
