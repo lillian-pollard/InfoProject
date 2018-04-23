bfe61f54470eedae484c30df56ad387937b8fd03
rangelandres <38231820+rangelandres@users.noreply.github.com>
2018-04-22 21:14:59 -0500

    f/s/tcourselist tables created

--- tables.sql
+++ tables.sql
@@ -9,23 +9,23 @@ DROP TABLE IF EXISTS student;
 DROP TABLE IF EXISTS tutor;
 DROP TABLE IF EXISTS account;
 
-
 --Account Table
 -- for all the columns/varibles that will ve eventually sent to the web app, use exactly the same words --
+--hashed pass is  null for now, must figure out when adding students, admin/prof doesn't have password or?
 CREATE TABLE account(
-    hawkid VARCHAR(20) NOT NULL,
+    hawkid VARCHAR(10) NOT NULL,
     name VARCHAR(120) NOT NULL,
-    hashedpass VARCHAR(120) NOT NULL,
-    student BIT NOT NULL,
-    tutor BIT NOT NULL,
-    administrator BIT NOT NULL,
-    faculty BIT NOT NULL,
+    hashedpass VARCHAR(120) NULL,
+    student BOOLEAN NOT NULL,
+    tutor BOOLEAN NOT NULL,
+    administrator BOOLEAN NOT NULL,
+    faculty BOOLEAN NOT NULL,
     PRIMARY KEY(hawkid)
                   );
 --Tutor table
 -- for all the columns/varibles that will ve eventually sent to the web app, use exactly the same words --
 CREATE TABLE tutor (
-    tutorid VARCHAR(20) NOT NULL,
+    tutorid VARCHAR(10) NOT NULL,
     name VARCHAR(120) NOT NULL,
     budget VARCHAR(120) NOT NULL,
     PRIMARY KEY(tutorid),
@@ -35,7 +35,7 @@ CREATE TABLE tutor (
 --Student table
 -- for all the columns/varibles that will ve eventually sent to the web app, use exactly the same words --
 CREATE TABLE student (
-    studentid VARCHAR(20) NOT NULL,
+    studentid VARCHAR(10) NOT NULL,
     name VARCHAR(120) NOT NULL,
     budget VARCHAR(120) NOT NULL,
     PRIMARY KEY(studentid),
@@ -45,15 +45,17 @@ CREATE TABLE student (
 -- for all the columns/varibles that will ve eventually sent to the web app, use exactly the same words --
 CREATE TABLE sessions (
     sessionid INT AUTO_INCREMENT,
-    sessiontime smalldatetime NOT NULL,
-    location VARCHAR(255) NOT NULL,
+    -- look at sql time/date data types. timestamp, datetime, etc
+    timeofsession VARCHAR(120) NOT NULL,
+    dateofsession VARCHAR(255) NOT NULL,
+    placeofsession VARCHAR(255) NOT NULL,
     tutorid VARCHAR(10) NOT NULL,
-    PRIMARY KEY(sessionid),
-    FOREIGN KEY(tutorid) REFERENCES tcourselist(tutorid)
+    PRIMARY KEY(sessionid)
+    
                   );
 --Course table
 CREATE TABLE course(
-    courseid VARCHAR(20) NOT NULL,
+    courseid INT NOT NULL,
     coursename VARCHAR(120) NOT NULL,
     PRIMARY KEY (courseid)
 );
@@ -62,7 +64,7 @@ CREATE TABLE course(
 CREATE TABLE problemset (
     problemid INT AUTO_INCREMENT,
     problemnotes VARCHAR(3000),
-	courseid VARCHAR(20) NOT NULL,
+	courseid INT NOT NULL,
     PRIMARY KEY (problemid),
     FOREIGN KEY(courseid) REFERENCES course(courseid)
 );
@@ -73,57 +75,41 @@ CREATE TABLE problemset (
 CREATE TABLE reservation(
     reservationid INT NOT NULL AUTO_INCREMENT,
     notes VARCHAR(3000),
-    cancel BIT NOT NULL,
+    cancel BOOLEAN NOT NULL,
     sessionid INT NOT NULL,
     problemid INT,
     studentid VARCHAR(10) NOT NULL,
     PRIMARY KEY(reservationid),
     FOREIGN KEY(sessionid) REFERENCES sessions(sessionID),
-    FOREIGN KEY(problemid) REFERENCES problemset(problemID)
+    FOREIGN KEY(problemid) REFERENCES problemset(ProblemID)    
                   );
+
+
 
 CREATE TABLE scourselist(
     hawkid VARCHAR(20) NOT NULL,
     courseid VARCHAR(20) NOT NULL,
     budget INT NOT NULL
-)
+);
 CREATE TABLE tcourselist(
     hawkid VARCHAR(20) NOT NULL,
     courseid VARCHAR(20) NOT NULL,
     budget INT NOT NULL
-)
+);
 CREATE TABLE fcourselist(
     hawkid VARCHAR(20) NOT NULL,
-    courseid VARCHAR(20) NOT NULL,
-)
+    courseid VARCHAR(20) NOT NULL
+);
+
 
 -- test cases for each data type
-INSERT INTO account VALUES('studenttutor','StudentTutor','$2a$12$f5t5UQZToCPUKNEF/KmtQek/B8M3u4GMe9kvnOxRALl4GNtDC3lSS',1,1,0,0);
-INSERT INTO account VALUES('studenttutor2','StudentTutor2','$2a$12$f5t5UQZToCPUKNEF/KmtQek/B8M3u4GMe9kvnOxRALl4GNtDC3lSS',1,1,0,0);
-INSERT INTO account VALUES('studenttutor3','StudentTutor3','$2a$12$f5t5UQZToCPUKNEF/KmtQek/B8M3u4GMe9kvnOxRALl4GNtDC3lSS',1,1,0,0);
-INSERT INTO account VALUES('studenttutor4','StudentTutor4','$2a$12$f5t5UQZToCPUKNEF/KmtQek/B8M3u4GMe9kvnOxRALl4GNtDC3lSS',1,1,0,0);
+INSERT INTO account VALUES('studenttutor','StudentTutor','$2a$12$f5t5UQZToCPUKNEF/KmtQek/B8M3u4GMe9kvnOxRALl4GNtDC3lSS',True,True,False,False);
+INSERT INTO account VALUES('student','Student','$2a$12$f5t5UQZToCPUKNEF/KmtQek/B8M3u4GMe9kvnOxRALl4GNtDC3lSS',True,False,False,False);
+INSERT INTO account VALUES('tutor','Tutor','$2a$12$f5t5UQZToCPUKNEF/KmtQek/B8M3u4GMe9kvnOxRALl4GNtDC3lSS',False,True,False,False);
+INSERT INTO account VALUES('admin','Admin','$2a$12$f5t5UQZToCPUKNEF/KmtQek/B8M3u4GMe9kvnOxRALl4GNtDC3lSS',False,False,True,False);
+INSERT INTO account VALUES('faculty','Faculty','$2a$12$f5t5UQZToCPUKNEF/KmtQek/B8M3u4GMe9kvnOxRALl4GNtDC3lSS',False,False,False,True);
+INSERT INTO account VALUES('facultyadmin','FacultyAdmin','$2a$12$f5t5UQZToCPUKNEF/KmtQek/B8M3u4GMe9kvnOxRALl4GNtDC3lSS',False,False,True,True);
+INSERT INTO account VALUES('arangl','Andres','$2a$12$NFWuIEO2J8sS/5tJz7.sEe895Do9IZ9I7RzcCeILTUlHHdO8Vhi2O',False,False,True,False);
 
-INSERT INTO account VALUES('student','Student','$2a$12$f5t5UQZToCPUKNEF/KmtQek/B8M3u4GMe9kvnOxRALl4GNtDC3lSS',1,0,0,0);
-INSERT INTO account VALUES('student2','Student2','$2a$12$f5t5UQZToCPUKNEF/KmtQek/B8M3u4GMe9kvnOxRALl4GNtDC3lSS',1,0,0,0);
-INSERT INTO account VALUES('student3','Student3','$2a$12$f5t5UQZToCPUKNEF/KmtQek/B8M3u4GMe9kvnOxRALl4GNtDC3lSS',1,0,0,0);
-INSERT INTO account VALUES('student4','Student4','$2a$12$f5t5UQZToCPUKNEF/KmtQek/B8M3u4GMe9kvnOxRALl4GNtDC3lSS',1,0,0,0);
 
-INSERT INTO account VALUES('tutor','Tutor','$2a$12$f5t5UQZToCPUKNEF/KmtQek/B8M3u4GMe9kvnOxRALl4GNtDC3lSS',0,1,0,0);
-INSERT INTO account VALUES('tutor2','Tutor2','$2a$12$f5t5UQZToCPUKNEF/KmtQek/B8M3u4GMe9kvnOxRALl4GNtDC3lSS',0,1,0,0);
-INSERT INTO account VALUES('tutor3','Tutor3','$2a$12$f5t5UQZToCPUKNEF/KmtQek/B8M3u4GMe9kvnOxRALl4GNtDC3lSS',0,1,0,0);
-INSERT INTO account VALUES('tutor4','Tutor4','$2a$12$f5t5UQZToCPUKNEF/KmtQek/B8M3u4GMe9kvnOxRALl4GNtDC3lSS',0,1,0,0);
-
-INSERT INTO account VALUES('admin','Admin','$2a$12$f5t5UQZToCPUKNEF/KmtQek/B8M3u4GMe9kvnOxRALl4GNtDC3lSS',0,0,1,0);
-
-INSERT INTO account VALUES('faculty','Faculty','$2a$12$f5t5UQZToCPUKNEF/KmtQek/B8M3u4GMe9kvnOxRALl4GNtDC3lSS',0,0,0,1);
-
-INSERT INTO account VALUES('facultyadmin','FacultyAdmin','$2a$12$f5t5UQZToCPUKNEF/KmtQek/B8M3u4GMe9kvnOxRALl4GNtDC3lSS',0,0,1,1);
-
-INSERT INTO course VALUES('CS:101','Intro to Computer Science');
-
-INSERT INTO scourselist VALUES('student','CS:101',3);
-INSERT INTO tcourselist VALUES('tutor','CS:101',3);
-INSERT INTO fcourselist VALUES('faculty','CS:101');
-
-                  
 
