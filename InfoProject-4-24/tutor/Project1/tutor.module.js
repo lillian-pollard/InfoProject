4201f4971a6e54839e99e8aee3a38d6844dfc1e4
sparkysoftball <38233092+sparkysoftball@users.noreply.github.com>
2018-04-24 10:58:57 -0500

    Add files via upload

---
+++ InfoProject-4-24/tutor/Project1/tutor.module.js
@@ -0,0 +1,9 @@
+/*
+* Creating a module for the entertainment app
+* We'll be using the name of the app 'entertainment' in the controller, and in the html (in the html element itself)
+*/
+(function() {
+    'use strict';
+    angular.module("tutor", []);
+}
+)();
