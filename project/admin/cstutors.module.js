be012b001fc2323096f4e44ea5cfc23ea660fcbc
Niebuhr <sniebuhr@serv42.divms.uiowa.edu>
2018-05-03 11:03:52 -0500

    Trying to clean up git

--- project/admin/cstutors.module.js
+++
@@ -1,9 +0,0 @@
-/*
-* Creating a module for the soccer app
-* We'll be using the name of the app 'soccer' in the controller, and in the html (in the html element itself)
-*/
-(function() {
-    'use strict';
-    angular.module("cstutors", []);
-}
-)();
--- project/tutor.module.js
+++
@@ -1,9 +0,0 @@
-/*
-* Creating a module for the entertainment app
-* We'll be using the name of the app 'entertainment' in the controller, and in the html (in the html element itself)
-*/
-(function() {
-    'use strict';
-    angular.module("tutor", []);
-}
-)();
