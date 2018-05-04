43b80bf337e343338008e02a764fbe144d575b7a
sparkysoftball <38233092+sparkysoftball@users.noreply.github.com>
2018-05-04 13:04:26 -0500

    Delete tutor.sql

--- tutor.sql
+++
@@ -1,7 +0,0 @@
-DROP TABLE IF EXISTS tutor;
--- for all the columns/varibles that will ve eventually sent to the web app, use exactly the same words --
-CREATE TABLE tuotr(
-    tutorid INT NOT NULL AUTO_INCREMENT,
-    budget VARCHAR(120) NOT NULL,
-    PRIMARY KEY(tutorid)
-                  );
