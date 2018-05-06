a0b5d9c2f482fd14fd88b15d337f29108a319cea
Pollard <pollrd@serv42.divms.uiowa.edu>
2018-05-06 12:31:20 -0500

    remove duplicate folder

--- informatics-project/cstutors.module.js
+++
@@ -1,9 +0,0 @@
-/*
-* Creating a module for the soccer app
-* We'll be using the name of the app 'cstutors' in the controller, and in the html (in the html element itself)
-*/
-(function() {
-    'use strict';
-    angular.module("cstutors", []);
-}
-)();
